5d5c9bd506a71b01f5001950d5db1d0b09508501
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2017-10-20 15:29:51 +0200

    remove classes

--- ContaoManager/Plugin.php
+++ ContaoManager/Plugin.php
@@ -3,19 +3,12 @@
 namespace con4gis\TrackingAndroidBundle\ContaoManager;
 
 use con4gis\ApiBundle\Con4gisApiBundle;
-use con4gis\CoreBundle\con4gisCoreBundle;
-use con4gis\GroupsBundle\con4gisGroupsBundle;
 use con4gis\TrackingAndroidBundle\con4gisTrackingAndroidBundle;
 use con4gis\TrackingBundle\con4gisTrackingBundle;
-use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Config\ConfigInterface;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
-use Contao\ManagerPlugin\Routing\RoutingPluginInterface;
-use Symfony\Component\Config\Loader\LoaderResolverInterface;
-use Symfony\Component\CssSelector\Parser\ParserInterface;
-use Symfony\Component\HttpKernel\KernelInterface;
 
 class Plugin implements BundlePluginInterface
 {
